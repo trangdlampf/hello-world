70a38360794dc631fd71ccc87ca6e5b30d6fb5ae
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2018-12-07 18:12:39 -0500

    Create Home.vue

---
+++ vue_routeCli/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Home">
+    <h1>Home</h1>
+    <p>This is the content of my Homepage.</p>
+  </div>
+</template>
+
+<!-- home may have to be Home -->
+<script>
+export default {
+  name: "Home"
+};
+</script>
