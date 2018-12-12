e671d122894963a3f9d29d61f762089c839eda55
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2018-12-12 16:41:44 -0500

    create About.vue

---
+++ vue_routeCli/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+	<p>Here is the content of the about page.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "about" // about is name of component
+};
+</script>
+
+<style>
+
+.contact {
+  font-size: 12px;
+}
+
+</style>
