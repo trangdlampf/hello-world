913a6046daa109eb1ba29f2e03fc399955a68732
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2018-12-12 16:41:06 -0500

    create Contact.vue

---
+++ vue_routeCli/views/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="contact">
+    <h1>Contact</h1>
+	<p>Here is the content of the contact page.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "contact" // contact is name of component
+};
+</script>
+
+<style>
+
+.contact {
+  font-size: 12px;
+}
+
+</style>
